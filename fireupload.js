c67cf0bb73d1de9502148409f4be073f2fcbf9b1
ahmed <webeasystep@gmail.com>
2023-07-08 12:45:53 +0300

    latest work example 1-drag and drop to sort fixed 2- preload files works good

--- fireupload.js
+++ fireupload.js
@@ -1,201 +1,161 @@
-$(document).ready(function() {
-    var $dropzone = $('#dropzone');
-    var $preview = $('#preview');
-    var $addIcon = $('#add-icon');
-    var files = [];
+class FireUploader {
+    constructor(options = {}) { // Default to an empty object if no argument is passed
+        this.$dropzone = $('#dropzone');
+        this.$preview = $('#preview');
+        this.$addIcon = $('#add-icon');
+        this.files = options.files || [];
 
-    $dropzone.on('dragover', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).addClass('dragover');
-    });
+        this.init();
+        this.handlePreloadedFiles();
+    }
 
-    $dropzone.on('dragenter', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).addClass('dragover');
-    });
+    init() {
 
-    $dropzone.on('dragleave', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).removeClass('dragover');
-    });
-
-    $dropzone.on('drop', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).removeClass('dragover');
-        $('.border-box').remove();
-
-        var droppedFiles = event.originalEvent.dataTransfer.files;
-        handleFiles(droppedFiles);
-    });
-
-    $('#fileinput').on('change', function() {
-        var selectedFiles = $(this).prop('files');
-        handleFiles(selectedFiles);
-    });
-
-    $addIcon.on('click', function() {
-        $('#fileinput').click();
-    });
-
-    $preview.on('click', '.remove-icon', function() {
-        var $previewItem = $(this).closest('.preview-item');
-        var index = $previewItem.index();
-        files.splice(index, 1);
-        $previewItem.remove();
-
-        if (files.length === 0) {
-            $addIcon.addClass('hidden');
-        }
-    });
-
-    $preview.on('click', '.zoom-icon', function() {
-        var $image = $(this).closest('.preview-item').find('img');
-        var imageDataUrl = $image.attr('src');
-        var $popup = $('<div>', { class: 'zoom-popup' });
-        var $zoomedImg = $('<img>', { class: 'zoomed-image', src: imageDataUrl });
-        var $closeIcon = $('<span>', { class: 'close-icon', html: '&times;' });
-
-        $popup.append($zoomedImg);
-        $popup.append($closeIcon);
-        $popup.appendTo('body');
-
-        $closeIcon.on('click', function() {
-            $popup.remove();
+        this.$dropzone.on('dragover', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            this.$dropzone.addClass('dragover');
         });
-    });
 
-    $preview.sortable({
-        containment: 'parent',
-        tolerance: 'pointer',
-        cursor: 'move',
-        placeholder: 'sortable-placeholder',
-        axis: 'x',
-        update: function(event, ui) {
-            var newOrder = $preview.sortable('toArray', { attribute: 'data-filename' });
-            files.sort(function(a, b) {
-                return newOrder.indexOf(a.name) - newOrder.indexOf(b.name);
-            });
-        },
-    });
+        this.$dropzone.on('dragenter', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            this.$dropzone.addClass('dragover');
+        });
 
-    $preview.on('mousedown', '.drag-drop-icon', function(event) {
-        $(this).closest('.preview-item').addClass('dragging');
-    });
+        this.$dropzone.on('dragleave', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            this.$dropzone.removeClass('dragover');
+        });
 
-    $preview.on('mouseup', '.drag-drop-icon', function(event) {
-        $(this).closest('.preview-item').removeClass('dragging');
-    });
+        this.$dropzone.on('drop', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            this.$dropzone.removeClass('dragover');
 
-    $preview.on('mousemove', '.drag-drop-icon', function(event) {
-        var $draggingItem = $(this).closest('.preview-item.dragging');
-        if ($draggingItem.length) {
-            var $sortablePlaceholder = $('<div>', { class: 'sortable-placeholder' });
-            var $prevItem = $draggingItem.prev();
-            var mouseY = event.pageY - $dropzone.offset().top;
-            var $nextItem;
+            const droppedFiles = event.originalEvent.dataTransfer.files;
+            this.handleFiles(droppedFiles);
+        });
 
-            if (mouseY < $draggingItem.outerHeight() / 2) {
-                $prevItem.before($sortablePlaceholder);
-                $nextItem = $sortablePlaceholder.next();
-            } else {
-                $draggingItem.after($sortablePlaceholder);
-                $nextItem = $sortablePlaceholder.prev();
-            }
+        $('#fileinput').on('change', (event) => {
+            this.handleFiles(event.target.files);
+        });
 
-            $sortablePlaceholder.before($draggingItem);
-            $sortablePlaceholder.remove();
+        this.$addIcon.on('click', () => {
+            $('#fileinput').click();
+        });
 
-            if ($nextItem.length) {
-                $draggingItem.insertAfter($nextItem);
-            }
-        }
-    });
+        this.$preview.sortable({
+            containment: 'parent',
+            tolerance: 'pointer',
+            cursor: 'move',
+            placeholder: 'sortable-placeholder',
+            handle: '.drag-drop-icon',
+            start: (event, ui) => {
+                ui.placeholder.height(ui.helper.outerHeight());
+            },
+            sort: (event, ui) => {
+                const containerScrollLeft = this.$preview.scrollLeft();
+                const containerWidth = this.$preview.width();
+                const itemWidth = ui.helper.outerWidth();
+                const leftOffset = ui.position.left - containerScrollLeft;
 
-    function handleFiles(selectedFiles) {
-        $.each(selectedFiles, function(index, file) {
+                if (leftOffset < 0 || leftOffset > containerWidth - itemWidth) {
+                    this.$preview.css('cursor', 'not-allowed');
+                } else {
+                    this.$preview.css('cursor', 'move');
+                }
+            },
+            stop: (event, ui) => {
+                this.$preview.css('cursor', 'auto');
+            },
+        });
+    }
+
+    handleFiles(selectedFiles) {
+        $.each(selectedFiles, (index, file) => {
             if (file.type.match('image.*')) {
-                var isDuplicate = false;
-                for (var i = 0; i < files.length; i++) {
-                    if (files[i].name === file.name && files[i].size === file.size) {
-                        isDuplicate = true;
-                        break;
-                    }
-                }
-
-                if (!isDuplicate) {
-                    var fileData = {
-                        file: file,
-                        dataUrl: null,
-                    };
-                    files.push(fileData);
-
-                    var reader = new FileReader();
-                    reader.onload = function(event) {
-                        var img = $('<img>', {
-                            src: event.target.result,
-                            alt: file.name,
-                        });
-
-                        var div = $('<div>', {
-                            class: 'preview-item',
-                            'data-filename': file.name,
-                            'data-index': files.length - 1,
-                        });
-
-                        var iconsDiv = $('<div>', { class: 'icons' });
-                        var removeIcon = $('<span>', {
-                            class: 'remove-icon',
-                            html: '<i class="fas fa-trash"></i>',
-                        });
-                        var zoomIcon = $('<span>', {
-                            class: 'zoom-icon',
-                            html: '<i class="fas fa-search-plus"></i>',
-                        });
-                        var dragDropIcon = $('<span>', {
-                            class: 'drag-drop-icon',
-                            html: '<i class="fas fa-arrows-alt"></i>',
-                        });
-
-                        iconsDiv.append(removeIcon);
-                        iconsDiv.append(zoomIcon);
-                        iconsDiv.append(dragDropIcon);
-
-                        div.append(img);
-                        div.append(iconsDiv);
-                        $preview.append(div);
-
-                        fileData.dataUrl = event.target.result;
-
-                        $('.drag-drop-icon').parent().sortable({
-                            containment: 'parent',
-                            tolerance: 'pointer',
-                            cursor: 'move',
-                            placeholder: 'sortable-placeholder',
-                            axis: 'x',
-                            handle: '.drag-drop-icon',
-                            update: function(event, ui) {
-                                var newOrder = $preview.sortable('toArray', { attribute: 'data-filename' });
-                                files.sort(function(a, b) {
-                                    return newOrder.indexOf(a.name) - newOrder.indexOf(b.name);
-                                });
-                            },
-                        });
-
-                    };
-
-                    reader.readAsDataURL(file);
-                }
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    this.addPreviewItem({
+                        dataUrl: event.target.result,
+                        name: file.name,
+                    });
+                };
+                reader.readAsDataURL(file);
             }
         });
+    }
 
-        if (files.length > 0) {
-            $addIcon.removeClass('hidden');
-        } else {
-            $addIcon.addClass('hidden');
+    handlePreloadedFiles() {
+        if (this.files.fileCount > 0) {
+            $.each(this.files.files, (index, file) => {
+                this.addPreviewItem({
+                    dataUrl: file.full_path,
+                    name: file.original_name,
+                });
+            });
         }
     }
-});
+
+    addPreviewItem(fileData) {
+        const img = $('<img>', {
+            src: fileData.dataUrl,
+            alt: fileData.name,
+        });
+
+        const div = $('<div>', {
+            class: 'preview-item',
+            'data-filename': fileData.name,
+        });
+
+        const iconsDiv = $('<div>', { class: 'icons' });
+        const removeIcon = $('<span>', {
+            class: 'remove-icon',
+            html: '<i class="fas fa-trash"></i>',
+        });
+        const zoomIcon = $('<span>', {
+            class: 'zoom-icon',
+            html: '<i class="fas fa-search-plus"></i>',
+        });
+        const dragDropIcon = $('<span>', {
+            class: 'drag-drop-icon',
+            html: '<i class="fas fa-arrows-alt"></i>',
+        });
+
+        iconsDiv.append(removeIcon);
+        iconsDiv.append(zoomIcon);
+        iconsDiv.append(dragDropIcon);
+
+        div.append(img);
+        div.append(iconsDiv);
+        this.$preview.append(div);
+
+        removeIcon.on('click', () => {
+            div.remove();
+        });
+
+        zoomIcon.on('click', () => {
+            const $popup = $('<div>', { class: 'zoom-popup' });
+            const $zoomedImg = $('<img>', {
+                class: 'zoomed-image',
+                src: fileData.dataUrl,
+            });
+            const $closeIcon = $('<span>', {
+                class: 'close-icon',
+                html: '&times;',
+            });
+
+            $popup.append($zoomedImg);
+            $popup.append($closeIcon);
+            $popup.appendTo('body');
+
+            $closeIcon.on('click', function () {
+                $popup.remove();
+            });
+        });
+
+        this.$addIcon.removeClass('hidden');
+    }
+}
