7f58694947e556b9db8829c9673c8d3cfbe55135
ahmed <webeasystep@gmail.com>
2023-07-10 14:33:23 +0300

    refactor code add max-height to handle overflow fix duplicated uploads add the ability to pass json hidden input value

--- fireupload.js
+++ fireupload.js
@@ -1,36 +1,64 @@
 class FireUploader {
     constructor({dropzoneId, inputName = 'file', multipleFiles = false, files = {files: [], fileCount: 0}} = {}) {
         $(".fireupload").each((index, element) => {
-            if ($(element).attr('id') == dropzoneId) {
-                const instance = Object.create(FireUploader.prototype);
-                instance.$dropzone = $(element);
-                instance.$preview = $('<div>', {class: 'preview'}).appendTo(instance.$dropzone);
-                instance.$fileInput = $('<input>', {
-                    type: 'file',
-                    name: inputName,
-                    class: 'choose-file-input',
-                    multiple: multipleFiles,
-                    id: 'fileInput' + instance.$dropzone.attr('id')
-                }).appendTo(instance.$dropzone);
-                instance.$chooseFileLabel = $('<label>', {
-                    for: 'fileInput' + instance.$dropzone.attr('id'),
-                    class: 'choose-file-label'
-                }).html('<i class="fas fa-upload"></i>Choose Files').appendTo(instance.$dropzone);
-                instance.$addIcon = $('<div>', {class: 'add-icon hidden'}).html('<i class="fas fa-plus"></i>').appendTo(instance.$dropzone);
-                instance.$dropzone.append($('<p>').text('Drag and drop files here'), $('<p>').text('Or'), instance.$chooseFileLabel, instance.$fileInput, instance.$addIcon);
-
-                instance.files = files;
-                instance.multipleFiles = multipleFiles;
-
-                instance.init();
-                instance.handlePreloadedFiles();
-
-                if (!multipleFiles) {
-                    instance.$addIcon.addClass('hidden');
-                    instance.$dropzone.find('.drag-drop-icon').hide();
-                }
+            if ($(element).attr('id') === dropzoneId) {
+                this.setUpInstance(element, inputName, multipleFiles, files);
             }
         });
+    }
+
+    setUpInstance(element, inputName, multipleFiles, files) {
+        this.initializeInstanceProperties(element, inputName, multipleFiles, files);
+        this.init();
+        this.handlePreloadedFiles();
+
+        if (!this.multipleFiles) {
+            this.$addIcon.addClass('hidden');
+            this.$dropzone.find('.drag-drop-icon').hide();
+        }
+    }
+
+    initializeInstanceProperties(element, inputName, multipleFiles, files) {
+        this.$dropzone = $(element);
+        this.$preview = this.createElementWithClass('div', 'preview').appendTo(this.$dropzone);
+        this.$fileInput = this.createFileInputElement(inputName, multipleFiles).appendTo(this.$dropzone);
+        this.$chooseFileLabel = this.createFileInputLabel().appendTo(this.$dropzone);
+        this.$addIcon = this.createAddIconElement().appendTo(this.$dropzone);
+        this.$dropzone.append(this.createDropzoneMessageElements());
+
+        this.files = files;
+        this.multipleFiles = multipleFiles;
+    }
+
+    createElementWithClass(elementType, className) {
+        return $('<' + elementType + '>', {class: className});
+    }
+
+    createFileInputElement(inputName, multipleFiles) {
+        const fileInputElementId = 'fileInput' + this.$dropzone.attr('id');
+        return $('<input>', {
+            type: 'file',
+            name: inputName,
+            class: 'choose-file-input',
+            multiple: multipleFiles,
+            id: fileInputElementId
+        });
+    }
+
+    createFileInputLabel() {
+        const labelFor = 'fileInput' + this.$dropzone.attr('id');
+        return $('<label>', {
+            for: labelFor,
+            class: 'choose-file-label'
+        }).html('<i class="fas fa-upload"></i>Choose Files');
+    }
+
+    createAddIconElement() {
+        return $('<div>', {class: 'add-icon hidden'}).html('<i class="fas fa-plus"></i>');
+    }
+
+    createDropzoneMessageElements() {
+        return [$('<p>').text('Drag and drop files here'), $('<p>').text('Or'), this.$chooseFileLabel, this.$fileInput, this.$addIcon];
     }
 
     init() {
@@ -84,8 +112,24 @@ class FireUploader {
                 onEnd: (event) => {
                     const item = event.item;
                     const newIndex = event.newIndex;
+                    const oldIndex = event.oldIndex;
                     const filename = item.getAttribute('data-filename');
-                    // Perform any necessary operations with the new index and filename
+
+                    // Move the file in the this.files.files array
+                    const movedFile = this.files.files.splice(oldIndex, 1)[0];
+                    this.files.files.splice(newIndex, 0, movedFile);
+
+                    // Update the order property of each file
+                    this.files.files.forEach((file, index) => {
+                        file.order = index + 1;
+                    });
+
+                    // Update the hidden inputs with the new file values
+                    this.$preview.find('.preview-item').each((index, previewItem) => {
+                        const $previewItem = $(previewItem);
+                        const $hiddenInput = $previewItem.find('.hidden-file-input');
+                        $hiddenInput.val(JSON.stringify(this.files.files[index]));
+                    });
                 }
             });
         }
@@ -167,14 +211,40 @@ class FireUploader {
     handleFiles(selectedFiles) {
         $.each(selectedFiles, (index, file) => {
             if (file.type.match('image.*')) {
-                const reader = new FileReader();
-                reader.onload = (event) => {
-                    this.addPreviewItem({
-                        dataUrl: event.target.result,
-                        name: file.name,
-                    });
-                };
-                reader.readAsDataURL(file);
+                // Check if the file is already in the array
+                if (this.files.files.some(existingFile => existingFile.raw_name === file.name)) {
+                    console.log(`The file ${file.name} already exists.`);
+                } else {
+                    const reader = new FileReader();
+                    reader.onload = (event) => {
+                        // Create an image to get the dimensions
+                        const img = new Image();
+                        img.onload = () => {
+                            const newFile = {
+                                size: (file.size / 1024).toFixed(3), // size of the file in kilobytes, rounded to three decimal places
+                                type: file.type, // MIME type of the file
+                                order: this.files.files.length + 1, // order of the file (assumes files array only contains current files)
+                                thumbs: [], // empty by default
+                                is_image: file.type.startsWith('image/') ? 1 : 0, // 1 if the file is an image, 0 if not
+                                raw_name: file.name, // raw name of the file
+                                dimension: img.width + '*' + img.height, // dimensions of the image, format: width*height
+                                extension: file.name.split('.').pop(), // file extension
+                                full_path: '', // by default empty because the added image is not uploaded yet
+                                original_name: file.name // the original name of the file
+                            };
+
+                            this.files.files.push(newFile);
+
+                            this.addPreviewItem({
+                                dataUrl: event.target.result,
+                                name: file.name,
+                                fileObject: newFile // pass the new file object to the function
+                            });
+                        };
+                        img.src = event.target.result;
+                    };
+                    reader.readAsDataURL(file);
+                }
             }
         });
     }
@@ -185,6 +255,7 @@ class FireUploader {
                 this.addPreviewItem({
                     dataUrl: file.full_path,
                     name: file.original_name,
+                    fileObject: file // pass the file object from preloaded files
                 });
             });
         }
@@ -201,7 +272,7 @@ class FireUploader {
             'data-filename': fileData.name,
         });
 
-        const iconsDiv = $('<div>', { class: 'icons' });
+        const iconsDiv = $('<div>', {class: 'icons'});
         const removeIcon = $('<span>', {
             class: 'remove-icon',
             html: '<i class="fas fa-trash"></i>',
@@ -222,15 +293,26 @@ class FireUploader {
         div.append(img);
         div.append(iconsDiv);
 
+        // Create the hidden file input
+        const hiddenFileInput = $('<input>', {
+            type: 'hidden',
+            name: this.$fileInput.attr('name'),
+            value: JSON.stringify(fileData.fileObject), // set the value to the corresponding file object
+            class: 'hidden-file-input'
+        }).appendTo(div);
+
         this.$preview.append(div);
 
         removeIcon.on('click', () => {
             div.remove();
+            // Remove the file from the files array
+            this.files.files = this.files.files.filter(file => file.original_name !== fileData.name);
             // Check if there are no previewed images to show the add icon
             if (this.$preview.find('.preview-item').length === 0) {
                 this.$addIcon.removeClass('hidden');
             }
         });
+
 
         zoomIcon.on('click', (event) => {
 
